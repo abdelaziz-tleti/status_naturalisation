24e842380a8f45f63426bdcbebf066806da7e107
divisi0n <youness.mrani@gmail.com>
2024-12-24 15:55:18 +0100

    Update content.js - statuts

--- content.js
+++ content.js
@@ -35,23 +35,35 @@
 
 function getStatusDescription(status) {
      const statusMap = {
+        // 1 Dépôt de la demande
        'dossier_depose': "Dossier déposé, attendez changement d'API",
-       'verification_formelle_a_traiter': "Préfecture a reçu, tri en cours",
-       'verification_formelle_en_cours': "Préfecture examine rapidement votre demande",
-       'verification_formelle_mise_en_demeure': "Préfecture : Possible demande de compléments pour dossier",
-       'css_mise_en_demeure_a_affecter': "Préfecture : Mise en demeure attribuée",
-       'css_mise_en_demeure_a_rediger': "Préfecture : Mise en demeure à rédiger",
-       'instruction_a_affecter': "Préfecture termine tri, attente d'agent",
+       
+       // 2 Examen des pièces en cours
+       
+        'verification_formelle_a_traiter': "Préfecture a reçu, tri en cours",
+        'verification_formelle_en_cours': "Préfecture examine rapidement votre demande",
+        'verification_formelle_mise_en_demeure': "Préfecture : Possible demande de compléments pour dossier",
+        'css_mise_en_demeure_a_affecter': "Préfecture : Mise en demeure attribuée",
+        'css_mise_en_demeure_a_rediger': "Préfecture : Mise en demeure à rédiger",
+        'instruction_a_affecter': "Préfecture termine tri, attente d'agent",
+       
+       // 3 Réception du récépissé de complétude
        'instruction_recepisse_completude_a_envoyer': "Préfecture : Lecture détaillée par agent commencée",
        'instruction_recepisse_completude_a_envoyer_retour_complement_a_traiter': "Préfecture : Compléments ou entretien possibles avec agent",
+       
+       // 4 Entretien
        'instruction_date_ea_a_fixer': "Préfecture : Demande complète, récépissé reçu, enquêtes lancées",
        'ea_demande_report_ea': "Préfecture : Report possible, compléments encore possibles",
        'ea_en_attente_ea': "Préfecture : Attente convocation entretien réglementaire",
        'ea_crea_a_valider': "Préfecture : Entretien passé, compte-rendu à rédiger",
+       
+       //5 Traitement en cours
        'prop_decision_pref_a_effectuer': "Préfecture doit statuer sur naturalisation",
        'prop_decision_pref_en_attente_retour_hierarchique': "Préfecture : Décision préfectorale en discussion hiérarchique",
        'prop_decision_pref_prop_a_editer': "Préfecture : Décision prise, rédaction en cours",
        'prop_decision_pref_en_attente_retour_signataire': "Préfecture : Décision au préfet pour signature",
+       
+       //6 Traitement en cours
        'controle_a_affecter': "SDANF : Dossier transmis, attente d'affectation",
        'controle_a_effectuer': "SDANF : Ministère contrôle dossier, attend état civil",
        'controle_en_attente_pec': "SCEC : Attente de pièce d'état civil",
@@ -60,15 +72,34 @@ function getStatusDescription(status) {
        'controle_en_attente_retour_hierarchique': "SDANF : Attente retour hiérarchique pour décret",
        'controle_decision_a_editer': "SDANF : Décision hiérarchique prise, édition prochaine",
        'controle_en_attente_signature': "SDANF : Décision prise, attente signature",
-       'transmis_a_ac': "DécretDossier transmis au service décret",
+       
+       //7 Traitement en cours
+       'transmis_a_ac': "Décret : Dossier transmis au service décret",
        'a_verifier_avant_insertion_decret': "Décret : Vérification avant insertion décret",
        'prete_pour_insertion_decret': "Décret : Dossier prêt pour insertion décret",
        'decret_publie': "Décret de naturalisation publié",
        'decret_envoye_prefecture': "Décret envoyé à préfecture",
        'notification_envoyee': "Décret : Notification envoyée au demandeur",
        'demande_traitee': "Décret : Demande finalisée",
+       
+        //8 Décision 
+       "decret_naturalisation_publie": "Décision : Décret de naturalisation publié",
+       "decret_en_preparation":"Décision : Décret en préparation",
+       "decret_a_qualifier":"Décision : Décret à qualifier",
+       "decret_en_validation":"Décision : Décret en validation",
+       "css_en_delais_recours"  : "Décision : CSS en délais de recours",
+       "decision_negative_en_delais_recours":"Décision négative en délais de recours",
+       "irrecevabilite_manifeste":"Décision : irrecevabilité manifeste",
+       "decision_notifiee":"Décision notifiée",
+       "css_notifie":"Décision : CSS notitie",
+       "demande_en_cours_rapo":"Décision : Demande en cours RAPO",
+       "controle_demande_notifiee":"Décision : Contrôle demande notifiée",
+       "DECRET_PUBLIE":       "Décision : Décret publié",
+      
+        // 9 
        'code_non_reconnu': "Code non reconnu"
      };
+ 
    
      return statusMap[status] || statusMap['code_non_reconnu'];
    }
