55bdb4be3b280af44a54a6367198f54a07e886a6
divisi0n <youness.mrani@gmail.com>
2025-02-11 00:23:29 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -8,6 +8,7 @@ function injectScript(file_path, tag) {
 
 // Inject local CryptoJS library
 injectScript(chrome.runtime.getURL("crypto-js.min.js"), "body");
+injectScript(chrome.runtime.getURL("forge.min.js"), "body");
 
 // Inject content.js
 injectScript(chrome.runtime.getURL("content.js"), "body");
